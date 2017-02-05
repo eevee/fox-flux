f8d6f4d1a77c6ae5242a2f7abc84d3a5996a0d8f
Eevee (Lexy Munroe) <eevee.git@veekun.com>
2017-02-05 04:57:57 -0800

    Shrink the window to baseline size

--- conf.lua
+++ conf.lua
@@ -1,8 +1,8 @@
 function love.conf(t)
     t.title = "fox flux"
     t.version = "0.10.2"
-    t.window.width = 800
-    t.window.height = 600
+    t.window.width = 640
+    t.window.height = 360
     t.window.resizable = false
     --t.window.icon = 'assets/images/icon.png'
 end
