2b0b8df3890afe25d356bd8de6990a3eb991008b
Eevee (Lexy Munroe) <eevee.git@veekun.com>
2017-02-28 23:39:31 -0800

    Add an icon

--- conf.lua
+++ conf.lua
@@ -1,8 +1,8 @@
 function love.conf(t)
-    t.title = "fox flux"
+    t.title = "fox flux [jam edition]"
     t.version = "0.10.2"
     t.window.width = 640
     t.window.height = 360
     t.window.resizable = true
-    --t.window.icon = 'assets/images/icon.png'
+    t.window.icon = 'assets/images/icon.png'
 end
